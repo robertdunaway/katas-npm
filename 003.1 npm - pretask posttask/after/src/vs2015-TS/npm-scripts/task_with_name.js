4930938f0393d156ba280b692c8f86b82ec2c835
Dmitry Karpov <lbvf50@gmail.com>
2016-02-16 23:22:32 +0600

    1) run `npm i` 2) fire `npm name_task` now name we take from console args 3) frie `npm start` ->  it will run presart start poststart 4) fire `npm stop` -> it will run prestop stop postso 5) fire `npm run task1` -> it wil run pretask1 task1 posttask1 6) fire `npm run allTasks` ->pretask1 task1 posttask1 task2 task3

---
+++ 003.1 npm - pretask posttask/after/src/vs2015-TS/npm-scripts/task_with_name.js
@@ -0,0 +1,10 @@
+﻿var timer = require('./timer.js');
+
+// Use Minimist to get name from a console args
+var argv = require('minimist')(process.argv.slice(2));
+var name = "Noname";
+
+if ('n' in argv) {
+    name = argv['n'];
+}
+timer(name);
